a68cfec6096287b92681bf2558c0886b3091d74d
sajibsaifullah <dusajibphy@gmail.com>
2023-04-29 12:06:33 +0600

    rating

--- src/pages/NewsCard/NewsCard.jsx
+++ src/pages/NewsCard/NewsCard.jsx
@@ -9,7 +9,8 @@ import {
   FaRegStar,
   FaStar,
 } from "react-icons/fa";
-import Rating from "react-rating";
+import { Rating } from '@smastrom/react-rating';
+import '@smastrom/react-rating/style.css';
 
 const NewsCard = ({ news }) => {
   const { _id, title, details, image_url, author, rating, total_view } = news;
@@ -43,15 +44,9 @@ const NewsCard = ({ news }) => {
         </Card.Text>
       </Card.Body>
       <Card.Footer className="text-muted d-flex">
-        <div className="flex-grow-1">
-          <Rating
-             placeholderRating={rating.number}
-             readonly
-             emptySymbol={<FaRegStar></FaRegStar>}
-             placeholderSymbol={<FaStar className="text-warning"></FaStar>}
-             fullSymbol={<FaStar></FaStar>}
-          ></Rating>
-          {rating?.number}
+        <div className="flex-grow-1 d-flex align-items-center">
+          <Rating style={{ maxWidth: 100 }} value={Math.round(rating?.number || 0)} readOnly />
+          <span className="ms-2">{rating?.number}</span>
         </div>
         <div>
           <FaEye></FaEye> {total_view}
